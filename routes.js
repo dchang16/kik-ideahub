921779a479787da911dfbdb127d3bc0225a720c7
David Chang <d26chang@gmail.com>
2014-01-25 13:15:39 -0500

    'test'

--- routes.js
+++ routes.js
@@ -43,7 +43,7 @@ module.exports = function (app) {
         })(req, res, next);
     });
 
-    app.get('/', function (req, res) {
+    app.get('', function (req, res) {
         res.render('index', { user : req.user });
     });
 
