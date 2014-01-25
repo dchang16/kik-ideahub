7eaf8df412d245ff582ae632c9027bd61bea7072
David Chang <d26chang@gmail.com>
2014-01-25 10:22:33 -0500

    restrict users to vote once

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,14 +1,23 @@
 var app = {
 	likeIdea: function() {
+		var id = $('#likeBtn').parent().attr('id');
+		if(localStorage.getItem(id) == 'true') {
+			document.getElementById('likeBtn').disabled='true';
+		}
 		$('html').on('click', '#likeBtn', function() {
-			var id = $(this).parent().attr('id');
+			if(localStorage.getItem(id) == 'true') {
+				console.log('disable');
+				document.getElementById('likeBtn').disabled='true';
+			}
 			console.log(id);
 			$.ajax({
 				type: 'GET',
 				url: '/like:' + id,
 				datatype: 'jsonp',
 				success: function() {
+					localStorage.setItem(id, 'true')
 					document.getElementById('votemsg').innerHTML = 'Thanks for voting!';
+					document.getElementById('likeBtn').disabled='true';
 				},
 				error: function() {
 					console.log('fail');
